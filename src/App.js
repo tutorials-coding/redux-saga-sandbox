9c45202c91bc8c3aef83c26e900d5cb777c04d7b
Vadim <korobeinikovv@mail.ru>
2021-07-26 22:29:24 +0300

    Dispatch action

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useDispatch } from 'react-redux'
+import Button from 'react-bootstrap/Button'
+
+import { USER_POSTS_FETCH_REQUESTED } from './store/actions'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-  return <div className="app__container"></div>
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch({ type: USER_POSTS_FETCH_REQUESTED, payload: { userId: 1 } })
+  }
+
+  return (
+    <div className="app__container">
+      <Button onClick={handleClick}>Get posts</Button>
+    </div>
+  )
 }
 
 export default App
