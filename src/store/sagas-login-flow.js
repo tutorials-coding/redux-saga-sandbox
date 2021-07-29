6387b61632342e7ccf4622a52aa4b5cf9acc3a83
Vadim <korobeinikovv@mail.ru>
2021-07-29 22:40:56 +0300

    Fixed missed LOGOUT (but contains another issue)

--- src/store/sagas-login-flow.js
+++ src/store/sagas-login-flow.js
@@ -1,16 +1,18 @@
-import { take, call, put } from 'redux-saga/effects'
+import { take, call, put, fork } from 'redux-saga/effects'
 import * as userApi from '../api/user'
 
 import { LOGIN_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT } from './actions'
 
 // This case is not covered by the flow:
-// LOGIN_REQUEST (waiting) -> LOGOUT (missed) -> LOGIN_SUCCESS
+// Now LOGOUT is not missed, but
+// LOGOUT in the middle of an userApi.login call - need to cancel the authorize process
 
 function* authorize(username, password) {
   try {
+    // fork, but how to get the result (token) - so, save token here
     const token = yield call(userApi.login, username, password)
     yield put({ type: LOGIN_SUCCESS, payload: { token } })
-    return token
+    yield call(userApi.saveToken, token)
   } catch (error) {
     yield put({ type: LOGIN_ERROR, payload: { error } })
   }
@@ -19,12 +21,11 @@ function* authorize(username, password) {
 function* loginFlow() {
   while (true) {
     const { payload } = yield take(LOGIN_REQUEST)
-    const token = yield call(authorize, payload.username, payload.password)
-    if (token) {
-      yield call(userApi.saveToken, token)
-      yield take(LOGOUT)
-      yield call(userApi.clearToken)
-    }
+    yield fork(authorize, payload.username, payload.password)
+
+    // watching for two concurrent actions
+    yield take([LOGOUT, LOGIN_ERROR])
+    yield call(userApi.clearToken)
   }
 }
 
