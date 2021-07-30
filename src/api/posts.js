de451e7e7f08c1ba5e0716f890e4d7da73e1db78
Vadim <korobeinikovv@mail.ru>
2021-07-30 22:10:06 +0300

    Add error example

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,4 +1,5 @@
 export const getUserPosts = (userId) => {
+  // throw new Error('Fetching posts failed')
   return fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}/posts`
   ).then((response) => response.json())
