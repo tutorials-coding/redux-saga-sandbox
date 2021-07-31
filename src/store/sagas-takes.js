1b4046ba32cc409d6ed1a59f6c6b043e50f3075c
Vadim <korobeinikovv@mail.ru>
2021-07-31 22:24:56 +0300

    Add more examples

--- src/store/sagas-takes.js
+++ src/store/sagas-takes.js
@@ -40,10 +40,41 @@ export function* userPostsFetchRequestedWatcherSaga() {
   // yield takeLatest(USER_POSTS_FETCH_REQUESTED, fetchUserPosts)
   // yield takeLeading(USER_POSTS_FETCH_REQUESTED, fetchUserPosts)
 
+  // just take example
   while (true) {
     const action = yield take(USER_POSTS_FETCH_REQUESTED)
     yield call(fetchUserPosts, action)
   }
+
+  // // takeEvery
+  // while (true) {
+  //   const action = yield take(USER_POSTS_FETCH_REQUESTED)
+  //   yield fork(fetchUserPosts, action)
+  // }
+
+  // // takeLeading
+  // while (true) {
+  //   const action = yield take(USER_POSTS_FETCH_REQUESTED)
+  //   yield call(fetchUserPosts, action)
+  // }
+
+  // // takeLatest
+  // let task
+  // while (true) {
+  //   const action = yield take(USER_POSTS_FETCH_REQUESTED)
+  //   if (task) {
+  //     yield cancel(task)
+  //   }
+  //   task = yield fork(fetchUserPosts, action)
+  // }
+
+  // skip n tasks
+  // while (true) {
+  //   yield take(USER_POSTS_FETCH_REQUESTED)
+  //   yield take(USER_POSTS_FETCH_REQUESTED)
+  //   const action = yield take(USER_POSTS_FETCH_REQUESTED)
+  //   yield fork(fetchUserPosts, action)
+  // }
 }
 
 export function* takeSaga() {
