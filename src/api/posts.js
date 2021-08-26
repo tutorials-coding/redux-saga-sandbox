2ecd146d58dce54b11e2884f89439e2b6d33fe30
Vadim <korobeinikovv@mail.ru>
2021-08-27 00:25:22 +0300

    Code cleanup

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,6 @@
 export const getUserPosts = (userId) => {
   // throw new Error('Fetching posts failed')
+  console.log('getUserPosts ===>')
   return fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}/posts`
   ).then((response) => response.json())
