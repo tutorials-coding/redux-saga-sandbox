d2effb7f26186b40aefe560e9a98aa0bc0b5020e
Vadim <korobeinikovv@mail.ru>
2021-08-28 22:37:32 +0300

    Write throttle based on fork, take, delay, actionChannel

--- src/store/sagas-throttle-debounce.js
+++ src/store/sagas-throttle-debounce.js
@@ -1,4 +1,12 @@
-import { fork, call, take, throttle } from 'redux-saga/effects'
+import {
+  fork,
+  call,
+  take,
+  throttle,
+  actionChannel,
+  delay,
+} from 'redux-saga/effects'
+import { buffers } from 'redux-saga'
 
 import { saveFriendlyName } from '../api/user'
 import { CHANGE_USERNAME } from './actions'
@@ -8,11 +16,23 @@ function* changeUsername(action) {
   yield call(saveFriendlyName, action.payload.username)
 }
 
+const throttle2 = (ms, pattern, task, ...args) =>
+  fork(function* () {
+    const throttleChannel = yield actionChannel(pattern, buffers.sliding(1))
+
+    while (true) {
+      const action = yield take(throttleChannel)
+      yield fork(task, ...args, action)
+      yield delay(ms)
+    }
+  })
+
 export function* sagaThrottleDebounce() {
   // compare with:
   // while (true) {
   //   const action = yield take(CHANGE_USERNAME)
   //   yield fork(changeUsername, action)
   // }
-  yield throttle(500, CHANGE_USERNAME, changeUsername)
+  // yield throttle(500, CHANGE_USERNAME, changeUsername)
+  yield throttle2(500, CHANGE_USERNAME, changeUsername)
 }
