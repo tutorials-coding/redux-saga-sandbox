5dae148c77d1352c4f299f66337f104d52751ebb
Vadim <korobeinikovv@mail.ru>
2021-08-29 12:54:54 +0300

    Get posts api from context

--- src/store/sagas-with-action-channel.js
+++ src/store/sagas-with-action-channel.js
@@ -1,15 +1,15 @@
-import { call, put, take, actionChannel } from 'redux-saga/effects'
+import { call, put, take, actionChannel, getContext } from 'redux-saga/effects'
 import { buffers } from 'redux-saga'
 import {
   USER_POSTS_FETCH_REQUESTED,
   USER_POSTS_FETCH_SUCCEEDED,
   USER_POSTS_FETCH_FAILED,
 } from './actions'
-import { getUserPosts } from '../api/posts'
 
 function* fetchUserPosts(action) {
   try {
-    const userPosts = yield call(getUserPosts, action.payload.userId)
+    const postsApi = getContext('postsApi')
+    const userPosts = yield call(postsApi.getUserPosts, action.payload.userId)
 
     yield put({
       type: USER_POSTS_FETCH_SUCCEEDED,
